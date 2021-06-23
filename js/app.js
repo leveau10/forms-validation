94a8ffacb0faa72fd3f460a28e92bc97ee039518
Leonardo Viana <leovir957@gmail.com>
2021-06-23 14:51:59 -0300

    Masks and CPF validation
    Uso de máscaras com bibliotecas de devs do github.
    Validação de CPF sem uso de bibliotecas.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ import { valida } from "./validacao.js";
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
+    if(input.dataset.tipo == 'preco'){ //input.dataset.tipo é o input do data attribute
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'BRL ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    } 
+
     input.addEventListener('blur', (evento) =>{
         valida(evento.target)
     })
